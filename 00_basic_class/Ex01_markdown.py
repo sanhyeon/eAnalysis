dceccd472326ba46abe4a319a4203b29810ebfa4
Sanghyeon Kim <gsa5681@gmail.com>
2022-12-14 19:40:15 +0900

    12-14 주피터 노트 커밋

---
+++ 00_basic_class/Ex01_markdown.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 쉘모드 변경
+# 
+# + code 쉘로 변경 : esc > y
+# + markdown 쉘로 변경 : esc > m
+
+# * 별표 하나는 번호 없는 목록
+# 
+# *별표로 감싸면 기울이기(주의 별표와 공백없이)*
+# 
+# **별표 2로 감싸면 굵게**
+# 
+# ***별표 3개?(굵게+기울이기)***
+# 
+# 가로선 ---
+# ---
+# 
+# 안녕하세요
+#     안녕하세요
+
+# <div align='center'>헬로우 월드<div>
+#     
+# <img src='dataset/puppy.jpg'>    
+#     
+# 
+# [링크걸기](http://www.google.com)
+#     
+# [![](https://www.google.com/logos/doodles/2022/seasonal-holidays-2022-6753651837109831.4-law.gif)](http://www.google.com)
+# 
+
+# In[2]:
+
+
+<table>
+</table>
+ 
+
+
+# In[ ]:
+
+
+import pandas
+
+p = pandas.read_csv('./dataset/subway_data1.csv', encoding='utf-8')
+
+
+# In[ ]:
+
+
+p
+
+
+# ## <font color='red'>[참고] 서울 지하철 </font>
+# 
+# <img src='dataset/map_subway.jpg'>
